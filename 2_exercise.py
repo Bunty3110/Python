cbe1efe371bf52c7871a1cbc7f5856baf061e941
BUNTY3110 <1ampraveenpardhi@gmail.com>
2026-05-31 14:37:28 +0530

    exercise 2 added

---
+++ 2_exercise.py
@@ -0,0 +1,27 @@
+# Exercise Cats Everywhere
+# Given the below class:
+
+class Cat:
+    species = 'mammal'
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# 1 Instantiate the Cat object with 3 cats.
+cat1 = Cat('Kitty', 5)
+cat2 = Cat('tommy', 6)
+cat3 = Cat('mimi', 3)
+
+# 2 Create a function that finds the oldest cat.
+def oldest_cat(*args):
+    return max(args)
+
+
+# 3 Print out: "The oldest cat is x years old.".
+# x will be the oldest cat age by using the function in #2
+
+def oldest_cat_print(x):
+    print(f'The oldest cat is {x} years old.')
+
+
